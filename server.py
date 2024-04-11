cdb547223f5f9ac8b8fa6505be85ddebf70c5765
Gyu-Park <kyesung8282@gmail.com>
2024-04-11 11:53:40 -0400

    first commit

---
+++ server.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask import render_template
+from flask import Response, request, jsonify
+app = Flask(__name__)
+
+data = []
+
+@app.route('/')
+def home():
+	return render_template('home.html')
+
+if __name__ == '__main__':
+   app.run(debug = True)
